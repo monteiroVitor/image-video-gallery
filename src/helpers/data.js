ed6568cafcac110ff345794fc63d2eeefb59e2a5
Vitor M Mendonça <monteiromendoncavitor@gmail.com>
2020-11-25 15:51:13 -0300

    Add: dados auxiliares

---
+++ src/helpers/data.js
@@ -0,0 +1,29 @@
+export const routes = [
+  {
+    id: 'rt0001',
+    url: '/',
+    text: 'home',
+  },
+  {
+    id: 'rt0002',
+    url: '/videos',
+    text: 'videos',
+  },
+];
+
+export const backgroundImages = {
+  photos: {
+    pageURL:
+      'https://pixabay.com/pt/photos/montanhas-%C3%A1rvores-nevoeiro-5750804/',
+    user: 'cwiela',
+    largeImageURL:
+      'https://cdn.pixabay.com/photo/2020/11/16/22/58/mountains-5750804_960_720.jpg',
+  },
+  videos: {
+    pageURL:
+      'https://pixabay.com/pt/photos/farol-torre-mar-oceano-costa-5762071/',
+    user: 'JonPauling',
+    largeImageURL:
+      'https://cdn.pixabay.com/photo/2020/11/20/16/10/lighthouse-5762071_960_720.jpg',
+  },
+};
